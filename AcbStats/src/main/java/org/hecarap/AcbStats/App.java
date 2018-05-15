a1c415148dc956c367958f8499c33a8af4140eaa
Héctor Arnau <hectorarnau@yahoo.es>
2018-05-15 22:42:52 +0200

    Creando proyecto maven

---
+++ AcbStats/src/main/java/org/hecarap/AcbStats/App.java
@@ -0,0 +1,13 @@
+package org.hecarap.AcbStats;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
